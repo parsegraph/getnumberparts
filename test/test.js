43759aa2c655a1ef2e7f6d1d1556cdd6781bcd88
Aaron Faanes <dafrito@gmail.com>
2021-12-06 23:23:48 -0600

    Implement getNumberParts - copied from parsegraph-node

--- test/test.js
+++ test/test.js
@@ -1,8 +1,24 @@
-import {assert} from "chai";
-import todo from "../dist/parsegraph-getnumberparts";
+import { assert } from "chai";
+import getNumberParts from "../dist/parsegraph-getnumberparts";
 
 describe("Package", function () {
-  it("works", ()=>{
-    assert.equal(todo(), 42);
+  it("works with positive one", () => {
+    assert.deepEqual(getNumberParts(1), { sign: 0, exponent: 0, mantissa: 1 });
+  });
+
+  it("works with negative one", () => {
+    assert.deepEqual(getNumberParts(-1), { sign: 1, exponent: 0, mantissa: 1 });
+  });
+
+  it("works with positive integers", () => {
+    assert.deepEqual(getNumberParts(2), { sign: 0, exponent: 1, mantissa: 1 });
+    assert.deepEqual(getNumberParts(3), { sign: 0, exponent: 1, mantissa: 1.5 });
+    assert.deepEqual(getNumberParts(4), { sign: 0, exponent: 2, mantissa: 1 });
+  });
+
+  it("works with negative integers", () => {
+    assert.deepEqual(getNumberParts(-2), { sign: 1, exponent: 1, mantissa: 1 });
+    assert.deepEqual(getNumberParts(-3), { sign: 1, exponent: 1, mantissa: 1.5 });
+    assert.deepEqual(getNumberParts(-4), { sign: 1, exponent: 2, mantissa: 1 });
   });
 });
